44aac87f93163500c395fe558f6399d096710bc5
Gideon Kariuki <gideon.kariuki@student.moringa.com>
2024-01-08 19:31:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    if (customerName) {
+        customerName = customerName.toUpperCase();
+    }
+}
+var bestCustomer;
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'fred';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'maybe fred';
+}
